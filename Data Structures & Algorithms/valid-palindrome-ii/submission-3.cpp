b76b975dac06509af1321612f4edbfcc66b95a8e
Mohammad Hasan <mohammad.hasan.prof@gmail.com>
2026-04-16 10:44:44 +0300

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+
+    bool isPalindrome(string & s, unsigned short l ,  unsigned short r)
+    {
+        while(l < r)
+        {
+            if(s[l] != s[r])
+                return false;
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        unsigned short last = s.length() - 1;
+        unsigned short first = 0;
+        unsigned short counter = 1;
+
+        while(first < last)
+        {
+            if(s[first] != s[last])
+            {
+                return isPalindrome(s , first + 1 , last) ||
+                    isPalindrome(s , first , last - 1);
+            }
+            first++;
+            last--;
+        }
+
+        return true;
+    }
+};
