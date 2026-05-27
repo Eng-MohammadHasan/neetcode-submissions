38e8293b7fda6326340e0a06ef3eb77d5838a9aa
Mohammad Hasan <mohammad.hasan.prof@gmail.com>
2026-05-27 20:58:33 +0300

    Add: crawler-log-folder - submission-1

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-1.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minOperations(vector<string>& logs) {
+        int depth = 0;
+
+        for(const string& operation : logs)
+        {   
+            if(operation == "../")
+            {
+                depth = max(0,depth - 1);
+            }
+            else if(operation != "./")
+            {
+                depth++;
+            }
+        }
+
+        return depth;
+    }
+};
