4abeddc22dc049964d65a6b9c3ea5164e769948f
Mohammad Hasan <mohammad.hasan.prof@gmail.com>
2026-05-29 16:44:17 +0300

    Add: find-all-numbers-disappeared-in-an-array - submission-1

---
+++ Data Structures & Algorithms/find-all-numbers-disappeared-in-an-array/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+
+        unordered_set<int> s(nums.begin(), nums.end());
+        vector<int> result;
+
+        for(int i = 1; i <= nums.size(); i++)
+        {
+            if(!s.count(i))
+                result.push_back(i);
+        }
+
+        return result;
+    }
+};
